c0b48255f50bc4473e95f65fd633f0748dad4e31
Tomomichi Sato <tsato@dcworks.jp>
2024-04-17 10:56:15 +0900

    Engineers Gate オリジナル課題：05_02_Webアプリケーションオリジナル課題4「SNSサイト」 Sampleアプリ。一旦枠だけ作ったのでコミット。

---
+++ src/main/java/jp/dcworks/app/addressnavigator/AddressNavigatorApplication.java
@@ -0,0 +1,13 @@
+package jp.dcworks.app.addressnavigator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressNavigatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressNavigatorApplication.class, args);
+	}
+
+}
